843d611a6aa445580ab638ba0795be315a950e5d
Breno Batista <breno_1993@outlook.com>
2023-07-24 00:28:57 -0300

    first layout
    falta algumas funcionalidades  como remover as tarefas pesquisar uma em especifico marcar como feita.
    Mas já e possível adicionar novas tarefas

---
+++ todo-react/src/components/Todo.jsx
@@ -0,0 +1,17 @@
+const Todo = ({ todo }) => {
+    return (
+      <div className="todo">
+        <div className="content">
+          <p>{todo.text}</p>
+          <p className="category">({todo.category})</p>
+        </div>
+        <div>
+          <button className="complete">completar</button>
+          <button className="remove">x</button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Todo;
+  
---
+++ todo-react/src/components/TodoForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const TodoForm = ({ addTodo }) => {
+    const [title, setTitle] = useState("")
+    const [category, setCategory] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!title || !category) return
+        // adicionar todo
+        addTodo(title, category)
+        //limpar campos
+        setTitle("")
+        setCategory("")
+        console.log(title, category);
+    }
+
+  return (
+    <div className='todo-form'>
+        <h2>Criar tarefa</h2>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Digite o título' value={title} onChange={(e) => setTitle(e.target.value)} />
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type="submit">Criar tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
