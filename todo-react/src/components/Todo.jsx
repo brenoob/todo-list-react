73ec351b5bc22b3615c1088eac7bd08833ded7b1
Breno Batista <breno_1993@outlook.com>
2023-07-24 15:19:24 -0300

    implementando funcionalidades no button complete
    modificando visualmente o DOM ao completar tarefa atraves do button completar

--- todo-react/src/components/Todo.jsx
+++ todo-react/src/components/Todo.jsx
@@ -1,12 +1,21 @@
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
     return (
-      <div className="todo">
+      <div className="todo"
+      style={{
+        textDecorationLine: todo.isCompleted ?"line-through" :"",
+        backgroundColor: todo.isCompleted ?"green" :"",
+        color: todo.isCompleted ?"#f8f8f2" :""
+      }}
+      >
         <div className="content">
           <p>{todo.text}</p>
           <p className="category">({todo.category})</p>
         </div>
         <div>
-          <button className="complete">completar</button>
+          <button className="complete"style={{
+        backgroundColor: todo.isCompleted ?"#6272a4" :"",
+            color: todo.isCompleted ?"#f8f8f2" :""
+          }} onClick={() => completeTodo(todo.id)}>completar</button>
           <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
         </div>
       </div>
